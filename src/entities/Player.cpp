0108467d7c1fc90beed238d62321796d156054a4
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2021-02-21 14:05:00 -0700

    Update Player.cpp

--- src/entities/Player.cpp
+++ src/entities/Player.cpp
@@ -6,7 +6,7 @@
 Player::Player() : Entity(EntityID::Player)
 {
     this->size = sf::Vector2f(50, 50);
-    this->speed = sf::Vector2f(20, 20);
+    this->speed = sf::Vector2f(550, 550);
     this->position = sf::Vector2f(Game::instance().get_settings()->width / 2, Game::instance().get_settings()->height / 2);
     this->shape.setSize(this->size);
     this->shape.setFillColor(sf::Color::Green);
