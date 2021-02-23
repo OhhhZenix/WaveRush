9c2a043021b119c78a6a525d03d6c5102607be32
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2021-02-23 11:58:29 -0700

    Update Utils.hpp

--- src/core/Utils.hpp
+++ src/core/Utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <random>
+
 #define SquaredValue(a) ((a) * (a))
 
 #define LerpValue(a, b, t) ((a) + ((t) * ((b) - (a))))
@@ -10,6 +12,10 @@
 
 #define ClampValue(a, b, c) (MinValue(MaxValue(c, a), b))
 
-inline float RandomF(float p_Min, float p_Max) {
-	
+inline float RandomF(float p_Min, float p_Max)
+{
+	std::random_device f_RandomDevice;
+	std::mt19937 f_MT(f_RandomDevice());
+	std::uniform_real_distribution<float> f_Dist(p_Min, p_Max);
+	return f_Dist(f_MT);
 }
