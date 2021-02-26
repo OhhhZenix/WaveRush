52fa8f5844116104270c010a262bf0e543694628
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2021-02-26 16:28:17 -0700

    Update toolchain cmake

--- cmake/VcpkgToolchain.cmake
+++ cmake/VcpkgToolchain.cmake
@@ -1,13 +1,11 @@
-## Use vcpkg for library management and as main toolchain
-## Try to find local installation first and download as backup
-if (DEFINED ENV{VCPKG_ROOT})
-    set(vcpkg_SOURCE_DIR $ENV{VCPKG_ROOT})
-else (DEFINED ENV{VCPKG_ROOT})
-    include(FetchContent)
-    message(STATUS "Local vcpkg instance not found, cloning repo https://github.com/microsoft/vcpkg")
-    FetchContent_Declare(vcpkg GIT_REPOSITORY "https://github.com/microsoft/vcpkg.git" GIT_TAG "master")
-    FetchContent_MakeAvailable(vcpkg)
-endif (DEFINED ENV{VCPKG_ROOT})
+# Use vcpkg for library management and as main toolchain
+# This cmake module require cmake version 3.16 or higher
 
-set(CMAKE_TOOLCHAIN_FILE "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
-message(STATUS "Using vcpkg toolchain file: ${CMAKE_TOOLCHAIN_FILE}")
+if (EXISTS ${CMAKE_BINARY_DIR}/vcpkg)
+    execute_process(COMMAND git pull WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/vcpkg)
+else()
+    execute_process(COMMAND git clone --branch master --recursive https://github.com/microsoft/vcpkg.git WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+endif()
+
+set(CMAKE_TOOLCHAIN_FILE "${CMAKE_BINARY_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake")
+message(STATUS "CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
