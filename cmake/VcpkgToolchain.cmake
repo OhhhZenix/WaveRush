41465b20fae7afbfd6874ca2e5da65167b318f95
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2021-03-01 12:24:53 -0700

    Update VcpkgToolchain.cmake

--- cmake/VcpkgToolchain.cmake
+++ cmake/VcpkgToolchain.cmake
@@ -1,15 +1,11 @@
 # Used for cmake vcpkg projects
 cmake_minimum_required(VERSION 3.16)
 
-if (DEFINED ENV{VCPKG_ROOT})
-    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE PATH "Path to vcpkg toolchain")
+if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/vcpkg)
+    execute_process(COMMAND git clone https://github.com/microsoft/vcpkg.git WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 else ()
-    if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/vcpkg)
-        execute_process(COMMAND git clone https://github.com/microsoft/vcpkg.git WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
-    else ()
-        execute_process(COMMAND git pull WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/vcpkg)
-    endif ()
-    set(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_BINARY_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE PATH "Path to vcpkg toolchain")
+    execute_process(COMMAND git pull WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/vcpkg)
 endif ()
 
+set(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_SOURCE_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake")
 message(STATUS "CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
