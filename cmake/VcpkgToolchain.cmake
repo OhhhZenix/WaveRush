dbb5302635dfb912867585cc4b96d11942797c50
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2021-03-01 12:15:18 -0700

    Vcpkg changes

---
+++ cmake/VcpkgToolchain.cmake
@@ -0,0 +1,13 @@
+# Used for cmake vcpkg projects
+cmake_minimum_required(VERSION 3.16)
+
+if (DEFINED ENV{VCPKG_ROOT})
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE PATH "Path to vcpkg toolchain")
+else ()
+    if (NOT EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/vcpkg)
+        execute_process(git clone https://github.com/microsoft/vcpkg.git WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR})
+    endif ()
+    set(CMAKE_TOOLCHAIN_FILE "${CMAKE_CURRENT_BINARY_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake" CACHE PATH "Path to vcpkg toolchain")
+endif ()
+
+message(STATUS "CMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
