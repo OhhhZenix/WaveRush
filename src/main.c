b30f2b9649a815291c9e1ace6754dbef18ffac02
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-23 09:10:01 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include "core/allocator.h"
+#include <stdint.h>
 
 int main(int argc, char* argv[]) {
-	ArenaAlloc(1 * 1024 * 1024);
+	uint32_t memInMegabytes = 1 * 1024 * 1024;
+	ArenaAlloc(memInMegabytes);
 	void* test = ArenaAlloc(1);
 	ArenaCleanup();
 	return 0;
