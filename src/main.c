547dcc9b425a157cafc72d3246afe9f653ec6aa8
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-24 22:19:37 -0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,40 +1,40 @@
 #include "core/allocator.h"
-#include "core/input.h"
-#include "core/renderer.h"
-#include "core/window.h"
+#include <nebula/nebula.h>
 #include <stdint.h>
 
 int main(int argc, char* argv[]) {
 	uint32_t memInMegabytes = 1 * 1024 * 1024;
 	ArenaInit(memInMegabytes);
 
-	WindowInit("Hello World", 640, 360);
-	RendererInit();
+	nebula_init();
+	nebula_window_init("Hello World", 640, 360);
+	nebula_renderer_init();
 
 	int32_t x = 0;
 	int32_t y = 0;
-	while (!WindowShouldClose()) {
-		WindowPollEvents();
-		if (IsKeyDown(4)) {
-			x -= 1;
-		}
-		if (IsKeyDown(7)) {
-			x += 1;
-		}
-		if (IsKeyDown(26)) {
-			y -= 1;
-		}
-		if (IsKeyDown(22)) {
-			y += 1;
-		}
+	while (!nebula_window_should_close()) {
+		nebula_window_poll_events();
+		// if (IsKeyDown(NEBULA_KEYCODE_A)) {
+		// 	x -= 1;
+		// }
+		// if (IsKeyDown(NEBULA_KEYCODE_D)) {
+		// 	x += 1;
+		// }
+		// if (IsKeyDown(NEBULA_KEYCODE_W)) {
+		// 	y -= 1;
+		// }
+		// if (IsKeyDown(NEBULA_KEYCODE_S)) {
+		// 	y += 1;
+		// }
 
-		RenderBegin((struct Color){ 0, .a = 255 });
-		DrawRectangle(x, y, 32, 32, (struct Color){ 255, 255, 255, 255 });
-		RenderEnd();
+		nebula_render_begin((struct Color){ 0, .a = 255 });
+		nebula_draw_rect(x, y, 32, 32, (struct Color){ 255, 255, 255, 255 });
+		nebula_render_end();
 	}
 
-	RendererCleanup();
-	WindowCleanup();
+	nebula_renderer_cleanup();
+	nebula_window_cleanup();
+	nebula_cleanup();
 	ArenaCleanup();
 
 	return 0;
