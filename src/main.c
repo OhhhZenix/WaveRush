17c00109844fb75aad3e31cb2eb6bbde1978cc3a
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-26 15:22:13 -0700

    add texture example

--- src/main.c
+++ src/main.c
@@ -1,21 +1,20 @@
 #include "core/allocator.h"
 #include <nebula/nebula.h>
 #include <stdint.h>
-#include <stdio.h>
 
 int main(int argc, char* argv[]) {
 	uint32_t mem_in_megabytes = 1 * 1024 * 1024;
 	arena_init(mem_in_megabytes);
 
-	nebula_init("Hello World", 640, 360);
+	nebula_init("Wave Rush", 640, 360);
 
 	float x = 0;
 	float y = 0;
+	uint32_t texture_id = nebula_load_texture("assets/textures/test.png");
 	while (!nebula_window_should_close()) {
 		nebula_window_poll_events();
 
 		double dt = nebula_get_frame_time_in_seconds();
-		printf("frame time: %f\n", dt);
 		if (nebula_is_key_down(NEBULA_KEY_A)) {
 			x -= 100 * dt;
 		}
@@ -30,7 +29,7 @@ int main(int argc, char* argv[]) {
 		}
 
 		nebula_render_begin((struct NebulaColor){ .a = 255 });
-		nebula_draw_rect(x, y, 32, 32, (struct NebulaColor){ 255 });
+		nebula_draw_texture(texture_id, x, y, 32, 32, (struct NebulaColor){ 255, 255, 255, 255 });
 		nebula_render_end();
 	}
 
