991167b8b6224f4b71d1d5f5dc375f209e374a72
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-01-26 12:11:54 -0700

    keys working + deltatime

--- src/main.c
+++ src/main.c
@@ -1,41 +1,41 @@
 #include "core/allocator.h"
 #include <nebula/nebula.h>
 #include <stdint.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
-	uint32_t memInMegabytes = 1 * 1024 * 1024;
-	ArenaInit(memInMegabytes);
+	uint32_t mem_in_megabytes = 1 * 1024 * 1024;
+	arena_init(mem_in_megabytes);
 
-	nebula_init();
-	nebula_window_init("Hello World", 640, 360);
-	nebula_renderer_init();
+	nebula_init("Hello World", 640, 360);
 
-	int32_t x = 0;
-	int32_t y = 0;
+	float x = 0;
+	float y = 0;
 	while (!nebula_window_should_close()) {
 		nebula_window_poll_events();
-		// if (IsKeyDown(NEBULA_KEYCODE_A)) {
-		// 	x -= 1;
-		// }
-		// if (IsKeyDown(NEBULA_KEYCODE_D)) {
-		// 	x += 1;
-		// }
-		// if (IsKeyDown(NEBULA_KEYCODE_W)) {
-		// 	y -= 1;
-		// }
-		// if (IsKeyDown(NEBULA_KEYCODE_S)) {
-		// 	y += 1;
-		// }
+
+		double dt = nebula_get_frame_time_in_seconds();
+		printf("frame time: %f\n", dt);
+		if (nebula_is_key_down(NEBULA_KEY_A)) {
+			x -= 100 * dt;
+		}
+		if (nebula_is_key_down(NEBULA_KEY_D)) {
+			x += 100 * dt;
+		}
+		if (nebula_is_key_down(NEBULA_KEY_W)) {
+			y -= 100 * dt;
+		}
+		if (nebula_is_key_down(NEBULA_KEY_S)) {
+			y += 100 * dt;
+		}
 
 		nebula_render_begin((struct Color){ 0, .a = 255 });
 		nebula_draw_rect(x, y, 32, 32, (struct Color){ 255, 255, 255, 255 });
 		nebula_render_end();
 	}
 
-	nebula_renderer_cleanup();
-	nebula_window_cleanup();
 	nebula_cleanup();
-	ArenaCleanup();
+	arena_cleanup();
 
 	return 0;
 }
