939eeef02f0cfd6d731e42e17f2edb1a26277c85
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-08 00:08:17 -0700

    Update Player.cpp

--- src/Entity/Player.cpp
+++ src/Entity/Player.cpp
@@ -1,1 +1,12 @@
+#include "Player.hpp"
 
+Player::Player(glm::vec2 startingPosition) {
+	this->position = startingPosition;
+	this->size = { 32, 32 };
+}
+
+Player::~Player() {}
+
+void Player::update() {}
+
+void Player::render() {}
