4355df682130e0c2b37e8fcf4ab770cd3309bf27
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-08 00:08:14 -0700

    Update Player.hpp

--- src/Entity/Player.hpp
+++ src/Entity/Player.hpp
@@ -1,1 +1,13 @@
+#pragma once
 
+#include <glm/vec2.hpp>
+
+struct Player {
+	glm::vec2 position;
+	glm::vec2 size;
+
+	Player(glm::vec2 startingPosition);
+	~Player();
+	void update();
+	void render();
+};
