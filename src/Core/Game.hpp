e65a842be274bb8353aba36d6e79c63cb2d7af73
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-08 00:08:18 -0700

    Update Game.hpp

--- src/Core/Game.hpp
+++ src/Core/Game.hpp
@@ -1,1 +1,15 @@
+#pragma once
 
+#include "Entity/Player.hpp"
+#include <SDL3/SDL.h>
+
+struct Game {
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	bool isRunning = true;
+	Player* player = nullptr;
+
+	Game();
+	~Game();
+	void run();
+};
