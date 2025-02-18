790950f9011a57cd06238be40e9acda018309d4e
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-02-17 19:45:32 -0700

    impl

--- src/Entity/BasicEnemy.cpp
+++ src/Entity/BasicEnemy.cpp
@@ -1,1 +1,9 @@
+#include "Entity/BasicEnemy.hpp"
 
+BasicEnemy::BasicEnemy(glm::vec2 startingPosition) {
+	this->position = startingPosition;
+}
+
+void BasicEnemy::Update(Game* game) {}
+
+void BasicEnemy::Render(Game* game) {}
--- src/Entity/BasicEnemy.hpp
+++ src/Entity/BasicEnemy.hpp
@@ -1,1 +1,14 @@
+#pragma once
 
+#include <glm/glm.hpp>
+
+struct Game;
+
+struct BasicEnemy {
+	glm::vec2 position = { 0, 0 };
+	glm::vec2 size = { 32, 32 };
+
+	BasicEnemy(glm::vec2 startingPosition = { 0, 0 });
+	void Update(Game* game);
+	void Render(Game* game);
+};
