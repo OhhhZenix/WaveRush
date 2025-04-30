2cab756880195b5f866d97bbb78a79660adf0f83
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-04-30 17:28:09 -0600

    Update Main.cpp

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,8 +1,11 @@
-#include "Core/Game.hpp"
+#include "WaveRush/Core/ArenaAllocator.hpp"
+#include "WaveRush/Core/Game.hpp"
 
 int main() {
-	Game* instance = new Game();
-	instance->Run();
-	delete instance;
+	ArenaAllocator allocator = ArenaAllocatorNew(1024 * 1024); // 1 MB arena
+	Game* instance = GameNew(&allocator);
+	GameRun(instance);
+	GameDelete(instance);
+	ArenaAllocatorDelete(&allocator);
 	return 0;
 }
