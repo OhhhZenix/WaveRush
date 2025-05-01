15f48a1b9257def01f8339d93747873b3e3087c0
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-04-30 19:28:34 -0600

    Update Vec2.hpp

--- src/Math/Vec2.hpp
+++ src/Math/Vec2.hpp
@@ -4,44 +4,36 @@
 
 template <typename T>
 struct Vec2 {
-	union {
-		struct
-		{
-			T X, Y;
-		};
-		__m128 Value;
-	};
+	T X, Y;
 
 	Vec2(T p_X = static_cast<T>(0), T p_Y = static_cast<T>(0)) {
 		X = p_X;
 		Y = p_Y;
 	}
 
-	Vec2(__m128 p_Value) {
-		Value = p_Value;
-	}
-
 	// Vec2 Operations
 	inline Vec2 operator+(const Vec2& p_Other) const {
-		return Vec2(_mm_add_ps(this->Value, p_Other.Value));
+		return Vec2(this->X + p_Other.X, this->Y + p_Other.Y);
 	}
 
-	inline Vec2 operator+=(const Vec2& p_Other) {
-		this->Value = _mm_add_ps(this->Value, p_Other.Value);
+	inline Vec2& operator+=(const Vec2& p_Other) {
+		this->X += p_Other.X;
+		this->Y += p_Other.Y;
 		return *this;
 	}
 
 	inline Vec2 operator-(const Vec2& p_Other) const {
-		return Vec2(_mm_sub_ps(this->Value, p_Other.Value));
+		return Vec2(this->X - p_Other.X, this->Y - p_Other.Y);
 	}
 
-	inline Vec2 operator-=(const Vec2& p_Other) {
-		this->Value = _mm_sub_ps(this->Value, p_Other.Value);
+	inline Vec2& operator-=(const Vec2& p_Other) {
+		this->X -= p_Other.X;
+		this->Y -= p_Other.Y;
 		return *this;
 	}
 
 	inline Vec2 operator*(const Vec2& p_Other) const {
-		return Vec2(_mm_mul_ps(this->Value, p_Other.Value));
+		return Vec2(this->X * p_Other.X, this->Y * p_Other.Y);
 	}
 
 	// Scalar Operations
