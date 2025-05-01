f2bcc12356b4dfb1499cf68d60aa35e72383c65d
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-04-30 19:34:21 -0600

    Update Vec2.hpp

--- src/Math/Vec2.hpp
+++ src/Math/Vec2.hpp
@@ -1,39 +1,47 @@
 #pragma once
 
 #include "Core/PCH.hpp"
+#include "SimdUtils.hpp"
 
 template <typename T>
 struct Vec2 {
-	T X, Y;
+	union {
+		struct {
+			T X, Y;
+		};
+		SimdType Value;
+	};
 
 	Vec2(T p_X = static_cast<T>(0), T p_Y = static_cast<T>(0)) {
 		X = p_X;
 		Y = p_Y;
 	}
 
+	Vec2(SimdType p_Value) {
+		Value = p_Value;
+	}
+
 	// Vec2 Operations
 	inline Vec2 operator+(const Vec2& p_Other) const {
-		return Vec2(this->X + p_Other.X, this->Y + p_Other.Y);
+		return Vec2(SimdAdd(this->Value, p_Other.Value));
 	}
 
 	inline Vec2& operator+=(const Vec2& p_Other) {
-		this->X += p_Other.X;
-		this->Y += p_Other.Y;
+		this->Value = SimdAdd(this->Value, p_Other.Value);
 		return *this;
 	}
 
 	inline Vec2 operator-(const Vec2& p_Other) const {
-		return Vec2(this->X - p_Other.X, this->Y - p_Other.Y);
+		return Vec2(SimdSub(this->Value, p_Other.Value));
 	}
 
 	inline Vec2& operator-=(const Vec2& p_Other) {
-		this->X -= p_Other.X;
-		this->Y -= p_Other.Y;
+		this->Value = SimdSub(this->Value, p_Other.Value);
 		return *this;
 	}
 
 	inline Vec2 operator*(const Vec2& p_Other) const {
-		return Vec2(this->X * p_Other.X, this->Y * p_Other.Y);
+		return Vec2(SimdMul(this->Value, p_Other.Value));
 	}
 
 	// Scalar Operations
