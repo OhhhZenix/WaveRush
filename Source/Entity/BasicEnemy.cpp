4b80cb34f748ad0a3ae9c7d44e858b8bc3b3d608
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2025-04-30 19:20:35 -0600

    refactor

--- Source/Entity/BasicEnemy.cpp
+++
@@ -1,9 +0,0 @@
-#include "Entity/BasicEnemy.hpp"
-
-BasicEnemy::BasicEnemy(glm::vec2 startingPosition) {
-	this->position = startingPosition;
-}
-
-void BasicEnemy::Update(Game* game) {}
-
-void BasicEnemy::Render(Game* game) {}
