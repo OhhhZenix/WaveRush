bacb6e0eda03009d983fadcf30d9b12f4781a37a
OhhhZenix <ahom725@gmail.com>
2025-10-30 13:52:47 -0600

    Update player.h

--- src/wr/entity/player.h
+++ src/wr/entity/player.h
@@ -8,5 +8,7 @@ struct wr_player {
 };
 
 void wr_player_init(wr_player* self, float x, float y);
+
 void wr_player_move(wr_player* self, float game_width, float game_height);
+
 void wr_player_draw(wr_player* self);
