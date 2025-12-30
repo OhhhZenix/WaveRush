bd9b18bc067ec6eb2ea0b45cfb7e74314299107e
OhhhZenix <ahom725@gmail.com>
2025-12-30 13:12:15 -0700

    make it work for windows

--- src/WaveRush/Utils.hpp
+++ src/WaveRush/Utils.hpp
@@ -7,14 +7,22 @@
 template<typename T>
 T GenerateRandomRange(T min, T max) {
     static std::mt19937 rng(
-        std::chrono::system_clock::now().time_since_epoch().count()
+        static_cast<unsigned>(
+            std::chrono::system_clock::now().time_since_epoch().count()
+        )
     );
 
-    using Distribution = typename std::conditional<
-        std::is_floating_point<T>::value,
-        std::uniform_real_distribution<T>,
-        std::uniform_int_distribution<T>>::type;
+    if constexpr (std::is_floating_point_v<T>) {
+        std::uniform_real_distribution<T> dist(min, max);
+        return dist(rng);
+    } else if constexpr (std::is_integral_v<T>) {
+        using DistType = std::conditional_t<(sizeof(T) < sizeof(int)), int, T>;
 
-    Distribution dist(min, max);
-    return dist(rng);
+        std::uniform_int_distribution<DistType> dist(
+            static_cast<DistType>(min),
+            static_cast<DistType>(max)
+        );
+
+        return static_cast<T>(dist(rng));
+    }
 }
