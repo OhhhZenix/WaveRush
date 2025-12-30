1386d2e702def771bed878191b40a9223b0ccbe1
OhhhZenix <ahom725@gmail.com>
2025-12-30 13:03:23 -0700

    make it generic

--- src/WaveRush/Utils.cpp
+++ src/WaveRush/Utils.cpp
@@ -1,11 +1,1 @@
 #include "WaveRush/Utils.hpp"
-#include <chrono>
-#include <random>
-
-
-int GenerateRandomIntRange(int min, int max) {
-    auto seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::mt19937 rng(seed);
-    auto dist = std::uniform_int_distribution<int>(min, max);
-    return dist(rng);
-}
--- src/WaveRush/Utils.hpp
+++ src/WaveRush/Utils.hpp
@@ -1,3 +1,20 @@
 #pragma once
 
-int GenerateRandomIntRange(int min, int max);
+#include <chrono>
+#include <random>
+#include <type_traits>
+
+template<typename T>
+T GenerateRandomRange(T min, T max) {
+    static std::mt19937 rng(
+        std::chrono::system_clock::now().time_since_epoch().count()
+    );
+
+    using Distribution = typename std::conditional<
+        std::is_floating_point<T>::value,
+        std::uniform_real_distribution<T>,
+        std::uniform_int_distribution<T>>::type;
+
+    Distribution dist(min, max);
+    return dist(rng);
+}
