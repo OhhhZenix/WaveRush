e29c13051231954905ba9b00956bc509aa67b52f
OhhhZenix <ahom725@gmail.com>
2026-01-01 17:21:59 -0700

    wip

--- src/WaveRush/EntityManager.cpp
+++ src/WaveRush/EntityManager.cpp
@@ -1,1 +1,17 @@
 #include "WaveRush/EntityManager.hpp"
+
+namespace WaveRush {
+
+EntityList& EntityManager::GetEntities() {
+    return this->entities;
+}
+
+void EntityManager::Update(Game& game) {
+    for (auto& entity : this->entities) {}
+}
+
+void EntityManager::Render() {
+    for (auto& entity : this->entities) {}
+}
+
+} // namespace WaveRush
--- src/WaveRush/EntityManager.hpp
+++ src/WaveRush/EntityManager.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <memory>
+#include <vector>
+
+namespace WaveRush {
+
+class Game;
+
+class Entity;
+
+using EntityList = std::vector<std::unique_ptr<Entity>>;
+
+class EntityManager {
+  public:
+    EntityList& GetEntities();
+    void Update(Game& game);
+    void Render();
+
+  private:
+    EntityList entities;
+};
+
+} // namespace WaveRush
