629575f4c71b44e4cff433bdd2912d1fc13b2a52
OhhhZenix <ahom725@gmail.com>
2026-03-09 15:34:59 -0600

    Update EntityManager.hpp

--- src/WaveRush/Entity/EntityManager.hpp
+++ src/WaveRush/Entity/EntityManager.hpp
@@ -18,39 +18,10 @@ struct EntityHandle {
 
 class EntityManager {
   public:
-    EntityManager(size_t max_entities) {
-        max_entities_ = max_entities;
-
-        for (size_t i = 0; i < max_entities; i++) {
-            ids_.push(i);
-            generations_[i] = 0;
-            alive_[i] = false;
-        }
-    }
-
-    auto createEntity() -> std::optional<EntityHandle> {
-        if (!ids_.empty()) {
-            EntityId id = ids_.front();
-            alive_[id] = true;
-            ids_.pop();
-            return EntityHandle {id, generations_[id]};
-        }
-        return std::nullopt;
-    }
-
-    auto isValid(EntityHandle handle) -> bool {
-        return handle.index < max_entities_
-            && handle.generations == generations_[handle.index]
-            && alive_[handle.index];
-    }
-
-    auto deleteEntity(EntityHandle handle) -> void {
-        if (isValid(handle)) {
-            alive_[handle.index] = false;
-            generations_[handle.index] += 1;
-            ids_.push(handle.index);
-        }
-    }
+    EntityManager(size_t max_entities);
+    auto createEntity() -> std::optional<EntityHandle>;
+    auto isValid(EntityHandle handle) -> bool;
+    auto deleteEntity(EntityHandle handle) -> void;
 
   private:
     std::queue<EntityId> ids_;
