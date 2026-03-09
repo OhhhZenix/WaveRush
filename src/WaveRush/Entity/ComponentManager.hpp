2a81905ea3b4142a556bc6411523a710c8c15029
OhhhZenix <ahom725@gmail.com>
2026-03-09 13:21:54 -0600

    Update ComponentManager.hpp

--- src/WaveRush/Entity/ComponentManager.hpp
+++ src/WaveRush/Entity/ComponentManager.hpp
@@ -11,12 +11,12 @@ namespace WaveRush {
 class ComponentManager {
   public:
     template<typename T>
-    void registerComponent() {
+    auto registerComponent() -> void {
         components_.insert(SparseSet<size_t, T>());
     }
 
     template<typename T>
-    std::vector<T>* getComponentArray() {
+    auto getComponentArray() -> std::vector<T>* {
         auto sparse_set = components_.get<SparseSet<size_t, T>>();
         if (sparse_set) {
             return &sparse_set->data();
