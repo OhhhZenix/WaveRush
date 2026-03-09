88b993c9e9f2d53a1b0a087f61e99b08959c0d57
OhhhZenix <ahom725@gmail.com>
2026-03-09 12:53:01 -0600

    Update AnyMap.hpp

--- src/WaveRush/Core/AnyMap.hpp
+++ src/WaveRush/Core/AnyMap.hpp
@@ -9,7 +9,20 @@ namespace WaveRush {
 class AnyMap {
   public:
     template<typename T>
-    void add() {}
+    void insert(T item) {
+        std::type_index idx = typeid(T);
+        data_.insert({idx, std::any(std::move(item))});
+    }
+
+    template<typename T>
+    T* get() {
+        std::type_index idx = typeid(T);
+        auto it = data_.find(idx);
+        if (it != data_.end()) {
+            return std::any_cast<T>(&it->second);
+        }
+        return nullptr;
+    }
 
   private:
     std::unordered_map<std::type_index, std::any> data_;
