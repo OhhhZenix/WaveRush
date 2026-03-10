006e0378afafdfccee728cd340cf13e2b6e1aa8c
OhhhZenix <ahom725@gmail.com>
2026-03-09 20:57:34 -0600

    Update EntityManager.cpp

--- src/WaveRush/Entity/EntityManager.cpp
+++ src/WaveRush/Entity/EntityManager.cpp
@@ -4,19 +4,19 @@ namespace WaveRush {
 
 EntityManager::EntityManager(size_t max_entities) {
     max_entities_ = max_entities;
+    generations_.resize(max_entities, 0);
+    positions_.resize(max_entities);
 
     for (size_t i = 0; i < max_entities; i++) {
-        ids_.push(i);
-        generations_[i] = 0;
-        alive_[i] = false;
+        free_ids_.push(i);
     }
 }
 
 auto EntityManager::createEntity() -> std::optional<EntityHandle> {
-    if (!ids_.empty()) {
-        EntityId id = ids_.front();
-        alive_[id] = true;
-        ids_.pop();
+    if (!free_ids_.empty()) {
+        EntityId id = free_ids_.front();
+        free_ids_.pop();
+        used_ids_.push_back(id);
         return EntityHandle {id, generations_[id]};
     }
     return std::nullopt;
@@ -24,16 +24,28 @@ auto EntityManager::createEntity() -> std::optional<EntityHandle> {
 
 auto EntityManager::isValid(EntityHandle handle) -> bool {
     return handle.index < max_entities_
-        && handle.generations == generations_[handle.index]
-        && alive_[handle.index];
+        && handle.generations == generations_[handle.index];
 }
 
 auto EntityManager::deleteEntity(EntityHandle handle) -> void {
-    if (isValid(handle)) {
-        alive_[handle.index] = false;
-        generations_[handle.index] += 1;
-        ids_.push(handle.index);
+    if (!isValid(handle))
+        return;
+
+    generations_[handle.index] += 1;
+
+    for (size_t i = 0; i < used_ids_.size(); i++) {
+        if (used_ids_[i] == handle.index) {
+            used_ids_[i] = used_ids_.back();
+            used_ids_.pop_back();
+            break;
+        }
     }
+
+    free_ids_.push(handle.index);
+}
+
+auto EntityManager::getActiveEntities() -> std::vector<EntityId>& {
+    return used_ids_;
 }
 
 } // namespace WaveRush
