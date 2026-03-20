8eca20a1af5442dc240612009583851eae5dc457
OhhhZenix <ahom725@gmail.com>
2026-03-20 12:03:14 -0600

    Update Game.cpp

--- src/WaveRush/Game.cpp
+++ src/WaveRush/Game.cpp
@@ -7,19 +7,9 @@
 #include <ctime>
 
 #include "WaveRush/Arena.h"
+#include "WaveRush/Bouncer.h"
 #include "WaveRush/Entity.h"
 #include "WaveRush/Vec2f.h"
-
-void wr_spawn_bouncer(EntityManager* entity_manager) {
-    size_t bouncer = wr_create_entity(entity_manager);
-    entity_manager->alive[bouncer] = true;
-    entity_manager->kind[bouncer] = EntityKind_Bouncer;
-    entity_manager->position[bouncer].x = rand() % (GAME_WIDTH - 16);
-    entity_manager->position[bouncer].y = rand() % (GAME_HEIGHT - 16);
-    entity_manager->velocity[bouncer].x = (rand() % 2) * 2 - 1;
-    entity_manager->velocity[bouncer].y = (rand() % 2) * 2 - 1;
-    entity_manager->size[bouncer] = {16, 16};
-}
 
 void wr_game_init(Game* game, Arena* main_allocator) {
     game->entity_manager = (EntityManager*)
@@ -31,39 +21,16 @@ void wr_game_init(Game* game, Arena* main_allocator) {
     wr_spawn_bouncer(game->entity_manager);
 }
 
-void wr_bouncer_movement_system(EntityManager* entity_manager) {
-    for (size_t i = 0; i < entity_manager->count; i++) {
-        if (!entity_manager->alive[i]) {
-            continue;
-        }
-
-        if (entity_manager->kind[i] != EntityKind_Bouncer) {
-            continue;
-        }
-
-        Vec2f* pos = &entity_manager->position[i];
-        Vec2f* vel = &entity_manager->velocity[i];
-        Vec2f* size = &entity_manager->size[i];
-
-        if (pos->x < 0 || pos->x > GAME_WIDTH - size->x) {
-            vel->x *= -1;
-        }
-
-        if (pos->y < 0 || pos->y > GAME_HEIGHT - size->y) {
-            vel->y *= -1;
-        }
-    }
-}
-
 void wr_physics_system(EntityManager* entity_manager, float dt) {
     for (size_t i = 0; i < entity_manager->count; i++) {
         if (!entity_manager->alive[i]) {
             continue;
         }
 
-        vec2f_add(&entity_manager->position[i], &entity_manager->velocity[i]);
-        // entity_manager->position[i].x += entity_manager->velocity[i].x;
-        // entity_manager->position[i].y += entity_manager->velocity[i].y;
+        entity_manager->position[i] = vec2f_add(
+            &entity_manager->position[i],
+            &entity_manager->velocity[i]
+        );
     }
 }
 
