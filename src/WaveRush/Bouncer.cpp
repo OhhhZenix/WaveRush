f5b11d0a856a39e953e7e5f54b5711d1da84b724
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-05-17 11:06:26 -0600

    Update Bouncer.cpp

--- src/WaveRush/Bouncer.cpp
+++ src/WaveRush/Bouncer.cpp
@@ -11,10 +11,14 @@ void wr_spawn_bouncer(EntityManager* entity_manager) {
     size_t bouncer = wr_create_entity(entity_manager);
     entity_manager->alive[bouncer] = true;
     entity_manager->kind[bouncer] = EntityKind_Bouncer;
-    entity_manager->position[bouncer].x = rand() % (GAME_WIDTH - 16);
-    entity_manager->position[bouncer].y = rand() % (GAME_HEIGHT - 16);
-    entity_manager->velocity[bouncer].x = (rand() % 2) * 2 - 1;
-    entity_manager->velocity[bouncer].y = (rand() % 2) * 2 - 1;
+    entity_manager->position[bouncer] = Vec2f {
+        .x = static_cast<float>(rand() % (GAME_WIDTH - 16)),
+        .y = static_cast<float>(rand() % (GAME_HEIGHT - 16)),
+    };
+    entity_manager->velocity[bouncer] = Vec2f {
+        .x = static_cast<float>((rand() % 2) * 2 - 1),
+        .y = static_cast<float>((rand() % 2) * 2 - 1),
+    };
     entity_manager->size[bouncer] = {16, 16};
 }
 
