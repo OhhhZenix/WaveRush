6cd20a654cd3f097812c6fa2bb025f17bf2a4c82
OhhhZenix <ahom725@gmail.com>
2026-06-18 18:48:46 -0600

    remove sdl2

--- cmake/SDL2.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    SDL2
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL
-    GIT_TAG        release-2.30.12
-)
-
-FetchContent_MakeAvailable(SDL2)
--- cmake/SDL2_ttf.cmake
+++
@@ -1,9 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(
-    SDL2_ttf
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf
-    GIT_TAG        release-2.24.0
-)
-
-FetchContent_MakeAvailable(SDL2_ttf)
