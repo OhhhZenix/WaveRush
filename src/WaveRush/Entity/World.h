5ac9657b06fe7ff7256528d840cbd79edb5c36d6
OhhhZenix <ahom725@gmail.com>
2026-06-25 17:57:46 -0600

    Update World.h

--- src/WaveRush/Entity/World.h
+++ src/WaveRush/Entity/World.h
@@ -25,11 +25,11 @@ struct wr_entity_ref {
 };
 
 struct wr_world {
-  wr_queue<size_t> free_slots;
+  wr_queue free_slots;
   wr_entity* entities;    // actual entity
   uint32_t* generations;  // used for stale
-  bool* used;             // used for alive
-  size_t length;          // how many entities
+  bool* alive;            // used for alive
+  size_t capacity;        // how many entities
 };
 
 void wr_world_init(wr_world* world, wr_arena* arena, size_t max_entities);
