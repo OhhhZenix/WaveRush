b85df694b0d12e4bb9fd71296c032532e86cd61b
OhhhZenix <ahom725@gmail.com>
2026-06-25 17:56:01 -0600

    c style

--- src/WaveRush/Core/Queue.cpp
+++ src/WaveRush/Core/Queue.cpp
@@ -1,1 +1,35 @@
 #include "Queue.h"
+
+void wr_queue_init(wr_queue *queue, size_t capacity, size_t element_size,
+                   wr_arena *arena) {
+  queue->buffer = (char *)wr_arena_alloc(arena, element_size * capacity);
+  queue->element_size = element_size;
+  queue->capacity = capacity;
+  queue->size = 0;
+  queue->head = 0;
+  queue->tail = 0;
+}
+
+bool wr_queue_push(wr_queue *queue, const void *element) {
+  if (queue->size == queue->capacity) return false;
+
+  memcpy(queue->buffer + queue->tail * queue->element_size, element,
+         queue->element_size);
+
+  queue->tail = (queue->tail + 1) % queue->capacity;
+  queue->size++;
+
+  return true;
+}
+
+bool wr_queue_pop(wr_queue *queue, void *out) {
+  if (queue->size == 0) return false;
+
+  memcpy(out, queue->buffer + queue->head * queue->element_size,
+         queue->element_size);
+
+  queue->head = (queue->head + 1) % queue->capacity;
+  queue->size--;
+
+  return true;
+}
--- src/WaveRush/Core/Queue.h
+++ src/WaveRush/Core/Queue.h
@@ -1,49 +1,20 @@
 #pragma once
 
 #include <cstddef>
+#include <cstring>
 
 #include "WaveRush/Core/Arena.h"
 
-template <typename T>
 struct wr_queue {
-  T* data;
-  size_t length;
-  size_t count;
-  size_t head;
-  size_t tail;
+  char *buffer;
+  size_t element_size;
+  size_t capacity;  // maximum number of elements
+  size_t size;      // current number of elements
+  size_t head;      // next element to dequeue
+  size_t tail;      // next position to enqueue
 };
 
-template <typename T>
-void wr_queue_init(wr_queue<T>* queue, size_t length, wr_arena* arena) {
-  queue->data = (T*)wr_arena_alloc(arena, sizeof(T) * length);
-  queue->length = length;
-  queue->count = 0;
-  queue->head = 0;
-  queue->tail = 0;
-}
-
-template <typename T>
-bool wr_queue_push(wr_queue<T>* queue, T element) {
-  if (queue->count == queue->length) {
-    return false;  // full
-  }
-
-  queue->data[queue->tail] = element;
-  queue->tail = (queue->tail + 1) % queue->length;
-  ++queue->count;
-
-  return true;
-}
-
-template <typename T>
-bool wr_queue_pop(wr_queue<T>* queue, T* out) {
-  if (queue->count == 0) {
-    return false;  // empty
-  }
-
-  *out = queue->data[queue->head];
-  queue->head = (queue->head + 1) % queue->length;
-  --queue->count;
-
-  return true;
-}
+void wr_queue_init(wr_queue *queue, size_t capacity, size_t element_size,
+                   wr_arena *arena);
+bool wr_queue_push(wr_queue *queue, const void *element);
+bool wr_queue_pop(wr_queue *queue, void *out);
