62fa95391579c217db7bdcdd6b28734e3997adaf
OhhhZenix <ahom725@gmail.com>
2026-06-30 11:57:02 -0600

    Update Game.h

--- src/WaveRush/Core/Game.h
+++ src/WaveRush/Core/Game.h
@@ -7,8 +7,13 @@
 struct wr_game {
   wr_world world;
   wr_arena arena;
+
   SDL_Window* window;
   SDL_GPUDevice* gpu;
+  SDL_GPUBuffer* vertex_buffer;
+  SDL_GPUTransferBuffer* transfer_buffer;
+  SDL_GPUGraphicsPipeline* graphics_pipeline;
+
   bool running;
 };
 
