882d56b6ef0e276319dcd61a28a666a8729a4b94
OhhhZenix <33815423+OhhhZenix@users.noreply.github.com>
2026-08-14 21:55:10 -0600

    update default shader

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -1,5 +1,4 @@
-#version 460
-
+#ifdef VERTEX_SHADER
 layout (location = 0) in vec3 a_position;
 layout (location = 1) in vec4 a_color;
 layout (location = 0) out vec4 v_color;
@@ -9,3 +8,15 @@ void main()
     gl_Position = vec4(a_position, 1.0f);
     v_color = a_color;
 }
+
+#endif
+
+#ifdef FRAGMENT_SHADER
+layout (location = 0) in vec4 v_color;
+layout (location = 0) out vec4 FragColor;
+
+void main()
+{
+    FragColor = v_color;
+}
+#endif
--- assets/shaders/fragment.glsl
+++
@@ -1,9 +0,0 @@
-#version 460
-
-layout (location = 0) in vec4 v_color;
-layout (location = 0) out vec4 FragColor;
-
-void main()
-{
-    FragColor = v_color;
-}
