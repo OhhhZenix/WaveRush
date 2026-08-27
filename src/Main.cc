fcc320eb2c675c46622e52738aa269b3fe29df3b
OhhhZenix <ahom725@gmail.com>
2026-08-27 14:41:55 -0600

    Update Main.cc

--- src/Main.cc
+++ src/Main.cc
@@ -50,8 +50,8 @@ sapp_desc sokol_main(int argc, char* argv[]) {
       .frame_cb = frame,
       .cleanup_cb = cleanup,
       .event_cb = event,
-      .width = 800,
-      .height = 600,
+      .width = 640,
+      .height = 360,
       .window_title = "Wave Rush",
   };
 }
